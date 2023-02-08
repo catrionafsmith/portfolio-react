69eab94a7eeb35d585928a850885ff2355b9aff0
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-08 23:54:42 +1100

    Add contact and work sections

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import HTML from '../assets/html.png'
 import CSS from '../assets/css.png'
-import JavaScript from '../assets/JavaScript.png'
+import JavaScript from '../assets/javascript.png'
 import ReactImg from '../assets/react.png'
 import Tailwind from '../assets/tailwind.png'
 import GitHub from '../assets/github.png'
@@ -10,16 +10,41 @@ import GitHub from '../assets/github.png'
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f]  text-gray-300 '>
         {/* Container */}
-        <div>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p>Experience</p>
-                <p>These are the technologies I've worked with</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Skills</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
             </div>
-            <div>
-                <img src={HTML} alt="html" />
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt="html" />
+                    <p>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt="css" />
+                    <p>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript" />
+                    <p>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={ReactImg} alt="react" />
+                    <p>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Tailwind} alt="tailwind" />
+                    <p>Tailwind</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GitHub} alt="github" />
+                    <p>GitHub</p>
+                </div>
             </div>
+            
         </div>
     </div>
   )
