2aef89d234e319908afb15fd3eab4778293df930
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-02-09 00:44:32 +1100

    Add scroll effect and links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/78167829-54bc-4f78-ab45-66f6523f9db7" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or email me - catriona.f.smith@gmail.com</p>
